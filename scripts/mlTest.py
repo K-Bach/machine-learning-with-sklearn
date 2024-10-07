0aa83be8cf9d2e7cc6bd0892833cfb8ad920983a
Ken-ser <karimbachir2001@gmail.com>
2024-10-08 01:55:20 +0200

    add user input

--- scripts/mlTest.py
+++ scripts/mlTest.py
@@ -1,16 +1,28 @@
 import pandas as pd
-from numpy import genfromtxt
 from sklearn import svm
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder, StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix, f1_score, precision_score, recall_score
+import os
 
 ##################################################
 
 # Load data using pandas
-data = pd.read_csv('../datasets/Train_data.csv')
+# List available datasets
+datasets_path = '../datasets'
+available_datasets = [f for f in os.listdir(datasets_path) if f.endswith('.csv')]
+print("Available datasets:")
+for idx, dataset in enumerate(available_datasets):
+    print(f"{idx}. {dataset}")
+
+# Let the user choose a dataset
+dataset_choice = int(input("Enter the number of the dataset you want to use: "))
+chosen_dataset = available_datasets[dataset_choice]
+
+# Load the chosen dataset
+data = pd.read_csv(os.path.join(datasets_path, chosen_dataset))
 
 # Separate features and target
 features = data.iloc[:, :-1]
@@ -20,6 +32,7 @@ class_value = data.iloc[:, -1]
 
 # Encode the target labels
 labels = LabelEncoder().fit_transform(class_value)
+class_names = LabelEncoder().fit(class_value).classes_
 
 # Identify the numerical and categorical features
 numerical_features = features.select_dtypes(include=['int64', 'float64']).columns
@@ -45,6 +58,7 @@ x_train, x_test, y_train, y_test = train_test_split(
     test_size=0.3, 
     random_state=0
     )
+# print("Number of 0s in y_test: ", (y_test == 0).sum())
 
 ##################################################
 
@@ -55,8 +69,15 @@ def print_stats_metrics(y_test, y_pred):
     print("Recall: ", recall_score(y_test, y_pred, average='macro'))
     print("F1 Score: ", f1_score(y_test, y_pred, average='macro'))
     cm = confusion_matrix(y_test, y_pred)
-    cm_df = pd.DataFrame(cm, index=[f'Actual {i}' for i in range(len(cm))], columns=[f'Predicted {i}' for i in range(len(cm[0]))])
+    cm_df = pd.DataFrame(cm, index=[f'Actual {class_names[i]}' for i in range(len(cm))], columns=[f'Predicted {class_names[i]}' for i in range(len(cm[0]))])
+    cm_df['Total Actual'] = cm_df.sum(axis=1)
+    cm_df.loc['Total Predicted'] = cm_df.sum(axis=0)
     print("Confusion Matrix: \n", cm_df)
+    # Save the confusion matrix to a file with a timestamp
+    # timestamp = pd.Timestamp.now().strftime('%Y%m%d_%H%M%S')
+    # cm_filename = f'confusion_matrix_{timestamp}.csv'
+    # cm_df.to_csv(cm_filename)
+    # print(f"Confusion matrix saved to {cm_filename}")
 
 ##################################################
 
