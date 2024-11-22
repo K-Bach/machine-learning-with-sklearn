ac66f5f747ef890c9a22978a9d5f3f0a637ac693
KBak <karimbachir2001@gmail.com>
2024-11-22 22:25:25 +0100

    Refactor toniotFullGenerator.py

--- toniotFullGenerator.py
+++ toniotFullGenerator.py
@@ -1,25 +1,6 @@
 import os
 import pandas as pd
 
-# Define the features and their data types
-string_features = [
-    "proto", "service", "conn_state", "dns_query", "ssl_version", "ssl_cipher", 
-    "ssl_subject", "ssl_issuer", "http_method", "http_uri", "http_version", 
-    "http_orig_mime_types", "http_resp_mime_types", "weird_name", "weird_addl", 
-    "http_user_agent"
-]
-int_features = [
-    "src_bytes", "dst_bytes", "missed_bytes", "src_pkts", "src_ip_bytes", 
-    "dst_pkts", "dst_ip_bytes", "dns_qclass", "dns_qtype", "dns_rcode", 
-    "http_trans_depth", "http_request_body_len", "http_response_body_len", 
-    "http_status_code"
-]
-float_features = ["duration"]
-boolean_features = [
-    "dns_AA", "dns_RD", "dns_RA", "dns_rejected", "ssl_resumed", 
-    "ssl_established", "weird_notice"
-]
-
 # Path to the datasets directory
 datasets_path = "datasets/toniot_full_network"
 
@@ -27,7 +8,7 @@ datasets_path = "datasets/toniot_full_network"
 csv_files = [f for f in os.listdir(datasets_path) if f.endswith('.csv')]
 
 # Initialize an empty list to hold DataFrames
-dataframes = []
+dataframes = pd.DataFrame()
 
 # Define columns to drop
 columns_to_drop = ['src_ip', 'src_port', 'dst_ip', 'dst_port', 'ts']
@@ -40,26 +21,13 @@ for csv_file in csv_files:
     # Read the CSV file
     data = pd.read_csv(os.path.join(datasets_path, csv_file))
     
-    # Check and replace non-integer values in 'src_bytes' column
-    # non_integer_values = data[~data['src_bytes'].apply(lambda x: str(x).isdigit() or str(x) == '-')]['src_bytes']
-    # if not non_integer_values.empty:
-    #     print(f"Non-integer values in {csv_file} 'src_bytes' column:")
-    #     print(non_integer_values)
-    #     data['src_bytes'] = data['src_bytes'].apply(lambda x: 0 if not str(x).isdigit() else int(x))
-    #     data.to_csv(os.path.join(datasets_path, csv_file), index=False)
-    #     print(f"Updated non-integer values in {csv_file} and saved the file.")
-    
     # Drop specified columns
     data = data.drop(columns=[col for col in columns_to_drop if col in data.columns])
     print(f"Dropped columns")
     
     # Append the DataFrame to the list
-    dataframes.append(data)
+    combined_data = pd.concat([dataframes, data], ignore_index=True)
     print(f"Appended data from {csv_file} to the list.")
-
-# Concatenate all DataFrames into a single DataFrame
-combined_data = pd.concat(dataframes, ignore_index=True)
-print("Concatenated all dataframes into a single dataframe.")
 
 # Ensure 'uid' column is not in the combined DataFrame
 if 'uid' in combined_data.columns:
